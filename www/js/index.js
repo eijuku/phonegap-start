ec2d724a2b263113ffab9a047e0d24147cc314de
Antti <antti.kuuskoski@taesmae.fi>
2012-12-09 00:32:46 +0200

    test

--- www/js/index.js
+++ www/js/index.js
@@ -46,6 +46,19 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
+    report: function(id) {
+		/*
+        // Report the event in the console
+        console.log("Report: " + id);
+
+        // Toggle the state from "pending" to "complete" for the reported ID.
+        // Accomplished by adding .hide to the pending element and removing
+        // .hide from the complete element.
+        document.querySelector('#' + id + ' .pending').className += ' hide';
+        var completeElem = document.querySelector('#' + id + ' .complete');
+        completeElem.className = completeElem.className.split('hide').join('');
+		*/
+    },
 	play: function() {
 		my_media = new Media("http://audio.ibeat.org/content/p1rj1s/p1rj1s_-_rockGuitar.mp3", 
 							 app.atEnd, app.eRRor);
